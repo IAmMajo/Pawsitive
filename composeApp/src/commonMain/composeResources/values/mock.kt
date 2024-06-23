1df19b141eeb2bc464266481bdef01379f391b02
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-06-24 00:25:03 +0200

    Added Mockdata of Alina

---
+++ composeApp/src/commonMain/composeResources/values/mock.kt
@@ -0,0 +1,67 @@
+val Vet = [
+{
+    name: "Dr. Andi Stosic"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Hunde",
+    clinic: "Kleintierpraxis Schnüffel",
+    address: "Carmerstraße 60, 45145 Neuss"
+    distance: "30.6 km"
+    diagnose: "5",
+    behandlung: "4",
+    empathie: "5"
+},
+{
+    name: "Dr. Susanne Sieslack"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Katzen",
+    clinic: "Tierklinik Sieslack",
+    address: "Flender Weg 3, 40472 Düsseldorf"
+    distance: "30.6 km"
+    diagnose: "5",
+    behandlung: "5",
+    empathie: "4"
+},
+{
+    name: "Dr. Stefan Figge-Sieslack"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Katzen",
+    clinic: "Tierklinik Sieslack",
+    address: "Flender Weg 3, 40472 Düsseldorf"
+    distance: "30.6 km"
+    diagnose: "5",
+    behandlung: "4",
+    empathie: "4"
+},
+{
+    name: "Dr. Julia Schneider"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Reptilien",
+    clinic: "Tierklinik Zoetic",
+    address: "Rheinstraße 144, 47203 Emmelsum"
+    distance: "65.1 km"
+    diagnose: "5",
+    behandlung: "5",
+    empathie: "3"
+},
+{
+    name: "Dr. Markus Bauschk"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Meerschweinchen",
+    clinic: "Tierklinik Millingen",
+    address: "Wardt-Straße 19, 46499 Brünen"
+    distance: "138.3 km"
+    diagnose: "4",
+    behandlung: "4",
+    empathie: "2"
+},
+{
+    name: "Dr. Martin Spennes"
+    imagePath: "composeApp/src/commonMain/composeResources/drawable/mockimage.jpeg",
+    specialization: "Gerbil",
+    clinic: "Kleintierpraxis Wittgens",
+    address: "Wittgensstraße 12, 47058 Duisburg"
+    distance: "14.2 km"
+    diagnose: "3",
+    behandlung: "2",
+    empathie: "3"
+}]
