30962488cdae44745949556a6e57b710c01c4279
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-07-07 14:21:32 +0200

    Added Filter Drawer to Search View

--- composeApp/src/commonMain/kotlin/ui/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/SearchScreen.kt
@@ -1,38 +1,128 @@
 package ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 import ui.components.ListEntryComponent
 import ui.components.Searchbar
 
 @Composable
-@Preview
+@OptIn(ExperimentalMaterial3Api::class)
 fun SearchScreen(navController: NavController) {
   val state = rememberScrollState()
   val viewModel = viewModel { SearchViewModel() }
   val uiState by viewModel.uiState.collectAsState()
-  LaunchedEffect(true) { viewModel.fetchResults() }
-  Column(
-      Modifier.fillMaxWidth().verticalScroll(state),
-      horizontalAlignment = Alignment.CenterHorizontally) {
-        Searchbar()
+  val drawerState = rememberDrawerState(DrawerValue.Closed)
+  val scope = rememberCoroutineScope()
 
-        if (uiState.loading) {
-          CircularProgressIndicator()
-        } else {
-          uiState.results.forEach { ListEntryComponent(it, navController) }
+  LaunchedEffect(true) { viewModel.fetchResults() }
+
+  ModalNavigationDrawer(
+    modifier = Modifier.background(Color.White),
+    drawerState = drawerState,
+    drawerContent = {
+      ModalDrawerSheet(
+        modifier = Modifier.background(Color.White)
+      ) {
+        Column(Modifier.padding(16.dp)) {
+          DropdownMenuComponent("Tierart")
+          Spacer(modifier = Modifier.height(16.dp))
+          DropdownMenuComponent("Umkreis")
+          Spacer(modifier = Modifier.height(16.dp))
+          DropdownMenuComponent("Straße")
         }
       }
+    },
+    content = {
+      Scaffold(
+        topBar = {
+          TopAppBar(
+            title = { Text("") },
+            navigationIcon = {
+              IconButton(onClick = {
+                scope.launch {
+                  drawerState.open()
+                }
+              }) {
+                Icon(Icons.Default.Menu, contentDescription = "Menu")
+              }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+              containerColor = Color.Transparent
+            )
+          )
+        }
+      ) {
+        Column(
+          Modifier
+            .fillMaxWidth()
+            .verticalScroll(state)
+            .padding(it),
+          horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+          Searchbar()
+          if (uiState.loading) {
+            CircularProgressIndicator()
+          } else {
+            uiState.results.forEach { ListEntryComponent(it, navController) }
+          }
+        }
+      }
+    }
+  )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownMenuComponent(label: String) {
+  var expanded by remember { mutableStateOf(false) }
+  var selectedText by remember { mutableStateOf("") }
+  val items = listOf("Item 1", "Item 2", "Item 3")
+
+  Column(
+  ) {
+    Text(text = label, fontWeight = FontWeight.SemiBold)
+    ExposedDropdownMenuBox(
+      expanded = expanded,
+      onExpandedChange = { expanded = !expanded }
+    ) {
+      TextField(
+        value = selectedText,
+        onValueChange = { selectedText = it },
+        readOnly = true,
+        label = { Text("Select Item") },
+        trailingIcon = {
+          ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+        },
+        modifier = Modifier.menuAnchor()
+      )
+      ExposedDropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false }
+      ) {
+        items.forEach { item ->
+          DropdownMenuItem(
+            onClick = {
+              selectedText = item
+              expanded = false
+            },
+            text = { Text(text = item) }
+          )
+        }
+      }
+    }
+  }
 }
